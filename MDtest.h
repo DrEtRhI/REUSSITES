953ed152243d1b1f0cb18a3b9507f52828f94c8a
Gaetan26 <gaetan.lagier@hotmail.fr>
2016-11-18 18:38:10 +0100

    Ajout MDtest.c .h

---
+++ MDtest.h
@@ -0,0 +1,12 @@
+/*--------------------------------------------------------------*/
+/* Reussites. Michel Bonin, Catherine Parent, octobre 2005, 
+   d'apres les algorithmes de Pierre-Claude Scholl              */
+/*--------------------------------------------------------------*/
+
+/* Montee Descente*/
+
+#include "AfficherTas.h"
+
+void ObserverMD(int);
+void AnalyserMD(int);
+	
